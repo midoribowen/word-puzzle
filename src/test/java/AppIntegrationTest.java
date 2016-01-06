16c14f73b5648c0ffb57c8a4225df5a128968d88
Midori Bowen <midori.p.bowen@gmail.com>
2016-01-06 09:19:17 -0800

    Add Integration Testing.

---
+++ src/test/java/AppIntegrationTest.java
@@ -0,0 +1,32 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppIntegrationTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+    return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("//insert page contents here for testing//")
+    //This tests is a page contains particular contents
+
+  @Test public void testNameHere() {
+    goTo("http://localhost:4567/");
+    fill("#tag").with("userInput");
+    submit(".btn");
+    assertThat(pageSource()).contains("//userInput//")
+    //This tests if a tagged fill is outputing correctly with the user input in the right place.
+  }
+  }
+}
